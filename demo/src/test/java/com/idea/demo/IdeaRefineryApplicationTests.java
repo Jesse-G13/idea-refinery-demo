901e3a4c895a815fe1f98bfedd82e79d82f0c101
Jesse <jessegonzales94@gmail.com>
2024-09-20 00:22:31 -0400

    First Commit

---
+++ demo/src/test/java/com/idea/demo/IdeaRefineryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idea.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeaRefineryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
